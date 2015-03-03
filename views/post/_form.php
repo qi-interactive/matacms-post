3756aac2c21d0577fe358ec7352813829ab60175
Marcin Wiatr <marcin@qi-interactive.com>
2015-03-03 18:21:39 +0000

    updated form
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- views/post/_form.php
+++ views/post/_form.php
@@ -1,23 +1,38 @@
 <?php
 
-use mata\widgets\DynamicForm;
-use matacms\post\assets\ModuleAsset;
+use yii\helpers\Html;
+use matacms\widgets\ActiveForm;
 
-ModuleAsset::register($this);
-
-echo DynamicForm::widget([
-	'model' => new matacms\post\models\Post(),
-	'fieldAttributes' => [
-	'Title' => [
-	'fieldType' => "textInput"
-	],
-	'Lead' => [
-		'fieldType' => "wysiwyg"
-	],
-	'Body' => [
-	'fieldType' => "wysiwyg"
-	]
-	]
-	]);
-
+/* @var $this yii\web\View */
+/* @var $model matacms\post\models\Post */
+/* @var $form ActiveForm */
+$locator = new \yii\di\ServiceLocator();
 ?>
+<div class="post">
+
+    <?php $form = ActiveForm::begin(); 
+    ?>
+
+        <?= $form->field($model, 'Title') ?>
+        <?= $form->field($model, 'Body')->wysiwyg() ?>
+        
+        <?= $form->field($model, 'Lead')->wysiwyg() ?>
+        <?= $form->field($model, 'Author') ?>
+
+
+        <?php if (Yii::$app->getModule("category") != null): ?>
+
+        	<?= $form->field($model, 'Lead')->category() ?>
+
+        <?php endif; ?>
+
+
+
+    	<?= $form->field($model, 'URI') ?>
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div><!-- qwdqwd -->
